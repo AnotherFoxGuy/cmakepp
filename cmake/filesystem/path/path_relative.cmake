3d5214cd1280fce9023b8473e8db88e9f80edc08
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-14 12:14:35 +0200

    :bug: Fixed filesystem tests on windows

--- cmake/filesystem/path/path_relative.cmake
+++ cmake/filesystem/path/path_relative.cmake
@@ -5,68 +5,63 @@
 # will result in ../dir3/dir4
 # returns nothing if transformation is not possible
 function(path_relative path_base path_rel)
-  set(args ${ARGN})
+    set(args ${ARGN})
 
-  path_qualify(path_base)
-  path_qualify(path_rel)
+    path_qualify(path_base)
+    path_qualify(path_rel)
 
+    if ("${path_base}" STREQUAL "${path_rel}")
+        return(".")
+    endif ()
 
-  if("${path_base}" STREQUAL "${path_rel}")
-    return(".")
-  endif()
+    path_split("${path_base}")
+    ans(base_parts)
 
-  path_split("${path_base}")
-  ans(base_parts)
+    path_split("${path_rel}")
+    ans(rel_parts)
 
-  path_split("${path_rel}")
-  ans(rel_parts)
+    set(result_base)
 
-  set(result_base)
+    set(first true)
 
-  set(first true)
+    while (true)
+        list_peek_front(base_parts)
+        ans(current_base)
+        list_peek_front(rel_parts)
+        ans(current_rel)
 
-  while(true)
-    list_peek_front(base_parts)
-    ans(current_base)
-    list_peek_front(rel_parts)
-    ans(current_rel)
+        if (NOT "${current_base}" STREQUAL "${current_rel}")
+            if (first)
+                return_ref(path_rel)
+            endif ()
+            break()
+        endif ()
+        set(first false)
 
+        path_combine("${result_base}" "${current_base}")
+        ans(result_base)
+        list_pop_front(base_parts)
+        list_pop_front(rel_parts)
+    endwhile ()
 
-    if(NOT "${current_base}" STREQUAL "${current_rel}")
-      if(first)
-        return_ref(path_rel)
-      endif()
-      break()
-    endif()
-    set(first false)
+    set(result_path)
 
-    path_combine("${result_base}" "${current_base}")
-    ans(result_base)
-    list_pop_front(base_parts)
-    list_pop_front(rel_parts)
-  endwhile()
+    foreach (base_part ${base_parts})
+        path_combine(${result_path} "..")
+        ans(result_path)
+    endforeach ()
 
-
-  set(result_path)
-
-  foreach(base_part ${base_parts})
-    path_combine(${result_path} "..")
+    path_combine(${result_path} ${rel_parts})
     ans(result_path)
-  endforeach()
 
+    if ("${result_path}" MATCHES "^\\/")
+        string_substring("${result_path}" 1)
+        ans(result_path)
+    endif ()
 
-  path_combine(${result_path} ${rel_parts})
-  ans(result_path)
-
-
-
-  if("${result_path}" MATCHES "^\\/")
-    string_substring("${result_path}" 1)
-    ans(result_path)
-  endif()
-
-  return_ref(result_path)
+    return_ref(result_path)
 endfunction()
+
 # transforms a path to a path relative to base_dir
 #function(path_relative base_dir path)
 #  path("${base_dir}")
