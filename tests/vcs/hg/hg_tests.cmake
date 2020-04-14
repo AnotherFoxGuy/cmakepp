da91899236702eff965c122b06b1e94edf0bb6dd
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-14 11:00:16 +0200

    :wrench: Disable failing hg test
    The repo is gone

--- tests/vcs/hg/hg_tests.cmake
+++ tests/vcs/hg/hg_tests.cmake
@@ -1,21 +1,16 @@
 function(test)
+    find_package(Hg)
+    if (NOT HG_FOUND)
+        message("Test Inconclusive, missing mercurial")
+        return()
+    endif ()
 
-  find_package(Hg)
-  if(NOT HG_FOUND)
-    message("Test Inconclusive, missing mercurial")
-    return()
-  endif() 
+    cd("${test_dir}")
 
-  cd("${test_dir}")
+    hg(--version --quiet)
+    ans(res)
 
-  hg(--version --quiet)
-  ans(res)
-
-  string(FIND "${res}" "${HG_VERSION_STRING}" idx)
-  assert(${idx} GREATER -1)
-
-
-
-
+    string(FIND "${res}" "${HG_VERSION_STRING}" idx)
+    assert(${idx} GREATER -1)
 
 endfunction()
