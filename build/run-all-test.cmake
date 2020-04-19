ab1ee1575a07e828c344c381c814c891a826f131
Edgar <Edgar@AnotherFoxGuy.com>
2020-04-19 12:47:24 +0200

    :boom: Removed package module

--- build/run-all-test.cmake
+++ build/run-all-test.cmake
@@ -5,15 +5,12 @@ include("${CMAKE_CURRENT_LIST_DIR}/../cmakepp.cmake")
 
 ## execute all tests in test directory
 if("$ENV{CMAKEPP_TEST_EXECUTE_PARALLEL}_" STREQUAL "true_" )
-  ## removed package source tests as they fail non deterministically
   test_execute_glob_parallel(
     "${CMAKE_CURRENT_LIST_DIR}/../tests/**.cmake"
-    "!${CMAKE_CURRENT_LIST_DIR}/../tests/package/package_source_tests/**.cmake"
     --recurse --no-status)
 else()
   test_execute_glob(
     "${CMAKE_CURRENT_LIST_DIR}/../tests/**.cmake"
-    "!${CMAKE_CURRENT_LIST_DIR}/../tests/package/package_source_tests/**.cmake"
     --recurse)
 endif()
 
